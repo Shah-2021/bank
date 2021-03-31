382062558da13550ddec8491429e9f8d74162ac9
Shah <oracle.sh.2020@gmail.com>
2021-03-31 15:57:38 +0500

    добавлена функция расчёта средней суммы платежа

---
+++ pkg/bank/stats/example_test.go
@@ -0,0 +1,38 @@
+package stats
+
+import (
+	"fmt"
+
+	"github.com/Shah-2021/bank/pkg/bank/types")
+
+
+func ExampleAvg() {
+	payments := []types.Payment{
+		{
+			Amount: 100,
+		},
+		{
+			Amount: 80,
+		},
+		{
+			Amount: 99,
+		},
+	}
+
+    fmt.Println(Avg(payments))
+
+	//Output: 93
+
+	
+}
+
+
+
+
+/*
+type Payment struct{
+	ID int
+	Amount Money
+	Category Category
+}
+*/
