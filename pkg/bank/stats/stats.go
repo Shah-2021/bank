ba84e26f7197e4418e2fb55f7d079f2bf9ac9377
Shah <oracle.sh.2020@gmail.com>
2021-03-31 16:14:53 +0500

    добавлена функция поиска суммы покупок по определённой категории

--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -14,3 +14,15 @@ func Avg(payments []types.Payment) types.Money {
 	}
 	return types.Money(avgSum/ind)
 }
+
+
+// TotalInCategory находит сумму покупок в определённой категории.
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money{
+	totalSum := types.Money(0)
+	for _, v := range payments {
+		if v.Category == category {
+			totalSum += v.Amount
+		}
+	}
+	return totalSum
+}
